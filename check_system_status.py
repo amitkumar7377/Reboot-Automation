5d213b4c596611a8b22d317260084c171850390f
Amit kumar Rout <51059369+amitkumar7377@users.noreply.github.com>
2024-05-24 13:03:37 +0530

    Update check_system_status.py

--- check_system_status.py
+++ check_system_status.py
@@ -1,20 +1,19 @@
 import subprocess
 import platform
 import time
+import os
 
 def is_system_rebooting():
     try:
-        # Get system uptime
         if platform.system() == "Windows":
             result = subprocess.run(['systeminfo'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
             uptime_info = result.stdout.split('\n')
             for line in uptime_info:
                 if "System Boot Time" in line:
                     boot_time_str = line.split(":")[1].strip()
-                    boot_time_str = boot_time_str.split(",")[0]  # Remove time portion
+                    boot_time_str = boot_time_str.split(",")[0]
                     boot_time = time.strptime(boot_time_str, "%m/%d/%Y")
                     current_time = time.time()
-                    # If the system was booted less than 5 minutes ago, consider it as rebooting
                     if current_time - time.mktime(boot_time) < 300:
                         return True
                     else:
@@ -32,16 +31,12 @@ def is_system_rebooting():
 
 def is_system_online(ip_address):
     try:
-        # Choose the appropriate command based on the platform
         if platform.system() == "Windows":
-            command = ['ping', '-n', '1', '-w', '1000', ip_address]  # Windows
+            command = ['ping', '-n', '1', '-w', '1000', ip_address]
         else:
-            command = ['ping', '-c', '1', '-W', '1', ip_address]  # Unix-like systems
+            command = ['ping', '-c', '1', '-W', '1', ip_address]
 
-        # Execute the ping command
         result = subprocess.run(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-        # Check the return code
         if result.returncode == 0:
             return True
         else:
@@ -50,11 +45,10 @@ def is_system_online(ip_address):
         print(f"An error occurred: {e}")
         return False
 
-
-ip_address = '192.168.1.14'
+ip_address = os.getenv('ip_address', '192.168.1.14')
 if is_system_online(ip_address) and not is_system_rebooting():
     print(f"The system {ip_address} is online.")
-    print("##vso[task.setvariable variable=SystemOnline]true")
+    os.environ['SystemStatus'] = 'online'
 else:
     print(f"The system {ip_address} is either offline or rebooting.")
-    print("##vso[task.setvariable variable=SystemOnline]false")
+    os.environ['SystemStatus'] = 'offline'
